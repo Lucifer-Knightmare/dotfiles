4f928e883e0fe0c62832ce0c1e80c13d8c4d8471
Lucifer-Knightmare <lucifer_knightmare@outlook.com>
2024-02-07 22:10:52 +0800

    Cleaning up & Add python to comform.nvim

--- dot_config/nvim/lua/custom/configs/conform.lua
+++ dot_config/nvim/lua/custom/configs/conform.lua
@@ -9,10 +9,12 @@ local options = {
 		html = { "prettier" },
 
 		sh = { "shfmt" },
+
+		python = { "yapf" },
 	},
 
-  -- adding same formatter for multiple filetypes can look too much work for some
-  -- instead of the above code you could just use a loop! the config is just a table after all!
+	-- adding same formatter for multiple filetypes can look too much work for some
+	-- instead of the above code you could just use a loop! the config is just a table after all!
 
 	-- format_on_save = {
 	--   -- These options will be passed to conform.format()
--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -392,25 +392,6 @@ local plugins = {
 		end,
 	},
 
-	-- {
-	--   "dccsillag/magma-nvim",
-	--   version = "*",
-	--   run = "UpdateRemotePlugins",
-	--   keys = {
-	--     { "<leader>mi", "<cmd>MagmaInit<CR>", desc = "This command initializes a runtime for the current buffer." },
-	--     { "<leader>mo", "<cmd>MagmaEvaluateOperator<CR>", desc = "Evaluate the text given by some operator." },
-	--     { "<leader>ml", "<cmd>MagmaEvaluateLine<CR>", desc = "Evaluate the current line." },
-	--     { "<leader>mv", "<cmd>MagmaEvaluateVisual<CR>", desc = "Evaluate the selected text." },
-	--     { "<leader>mc", "<cmd>MagmaEvaluateOperator<CR>", desc = "Reevaluate the currently selected cell." },
-	--     { "<leader>mr", "<cmd>MagmaRestart!<CR>", desc = "Shuts down and restarts the current kernel." },
-	--     {
-	--       "<leader>mx",
-	--       "<cmd>MagmaInterrupt<CR>",
-	--       desc = "Interrupts the currently running cell and does nothing if not cell is running.",
-	--     },
-	--   },
-	-- },
-
 	{
 		"kdheepak/lazygit.nvim",
 		-- optional for floating window border decoration
@@ -419,6 +400,11 @@ local plugins = {
 		},
 		lazy = false,
 	},
+
+  -- {
+  --   'dccsillag/magma-nvim',
+  --   build = ':UpdateRemotePlugins'
+  -- }
 
 	-- To make a plugin not be loaded
 	-- {
