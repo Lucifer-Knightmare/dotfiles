16a581aeab5636c1a8fba5b0147e8a8c6f3ba2f6
Lucifer-Knightmare <lucifer_knightmare@outlook.com>
2024-02-09 21:53:38 +0800

    image.nvim

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -148,7 +148,7 @@ local plugins = {
 				translate_problems = false,
 			},
 			cache = { update_interval = 60 * 60 },
-			-- image_support = true,
+			image_support = true,
 		},
 		-- lazy = false,
 		keys = {
@@ -167,10 +167,6 @@ local plugins = {
 		"wakatime/vim-wakatime",
 		lazy = false,
 	},
-
-	-- {
-	--   '3rd/image.nvim', lazy = false
-	-- },
 
 	{
 		"jghauser/papis.nvim",
@@ -415,17 +411,49 @@ local plugins = {
 
 	{
 		"https://gitlab.com/HiPhish/rainbow-delimiters.nvim.git",
+		branch = "master",
 		lazy = false,
-		-- config = function()
-		-- 	require("rainbow-delimiters.setup").setup({
-		-- 		query = {
-		-- 			[""] = "rainbow-delimiters",
-		-- 			-- lua = "rainbow-blocks",
-		-- 			-- latex = "rainbow-blocks",
-		-- 		},
-		-- 	})
-		-- end,
+		config = function()
+			require("rainbow-delimiters.setup").setup({
+				query = {
+					[""] = "rainbow-delimiters",
+					lua = "rainbow-blocks",
+					latex = "rainbow-blocks",
+				},
+			})
+		end,
 	},
+
+	{
+		-- see the image.nvim readme for more information about configuring this plugin
+		"3rd/image.nvim",
+		config = function()
+			package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
+			package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
+			require("image").setup()
+		end,
+		opts = {
+			backend = "kitty", -- whatever backend you would like to use
+			max_width = 100,
+			max_height = 12,
+			max_height_window_percentage = math.huge,
+			max_width_window_percentage = math.huge,
+			window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+			window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+		},
+	},
+
+	-- {
+	-- 	"benlubas/molten-nvim",
+	-- 	version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+	-- 	dependencies = { "3rd/image.nvim" },
+	-- 	build = ":UpdateRemotePlugins",
+	-- 	init = function()
+	-- 		-- these are examples, not defaults. Please see the readme
+	-- 		vim.g.molten_image_provider = "image.nvim"
+	-- 		vim.g.molten_output_win_max_height = 20
+	-- 	end,
+	-- },
 
 	-- To make a plugin not be loaded
 	-- {
