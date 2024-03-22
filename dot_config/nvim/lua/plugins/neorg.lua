3fb8095727409508b272d3a986eb438259cbd489
Lucifer Knightmare <lucifer_knightmare@outlook.com>
2024-03-22 18:16:29 +0800

    Neorg Tests

--- dot_config/nvim/lua/plugins/neorg.lua
+++ dot_config/nvim/lua/plugins/neorg.lua
@@ -1,31 +1,36 @@
 return {
-  "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
-  lazy = false, -- specify lazy = false because some lazy.nvim distributions set lazy = true by default
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("neorg").setup {
-      -- tag = "*",
-      -- configuration here
-      load = {
-        ["core.defaults"] = {},
-        ["core.dirman"] = {
-          config = {
-            workspaces = {
-              work = "~/notes/work",
-              home = "~/notes/home",
-              masters = "~/notes/masters",
-              personal_research = "~/notes/personal_research",
-            },
-          },
-        },
-        ["core.concealer"] = {
-          config = {
-            folds = false,
-            -- icon_present = "basic",
-          },
-        },
-      },
-    }
-  end,
+	"nvim-neorg/neorg",
+	build = ":Neorg sync-parsers",
+	-- lazy = false, -- specify lazy = false because some lazy.nvim distributions set lazy = true by default
+	lazy = true, -- enable lazy load
+	ft = "norg", -- lazy load on file type
+	cmd = "Neorg", -- lazy load on command
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("neorg").setup({
+			-- tag = "*",
+			-- configuration here
+			load = {
+				["core.defaults"] = {},
+				["core.dirman"] = {
+					config = {
+						workspaces = {
+							work = "~/notes/work",
+							home = "~/notes/home",
+							masters = "~/notes/masters",
+							personal_research = "~/notes/personal_research",
+						},
+					},
+				},
+				["core.concealer"] = {
+					config = {
+						folds = false,
+						-- icon_present = "basic",
+					},
+				},
+				["core.integrations.image"] = {},
+				["core.latex.renderer"] = {},
+			},
+		})
+	end,
 }
